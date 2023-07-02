a109a5b6506d2c53df7712b4dc16ec5ea6fbc038
GR1MR34P3R-1 <66636694+GR1MR34P3R-1@users.noreply.github.com>
2023-07-02 11:59:22 -0700

    main.py

--- main.py
+++ main.py
@@ -1,57 +1,88 @@
 import platform
+import tkinter as tk
+from tkinter import messagebox
+import qrcode
+import wifi
 
-# Import libraries based on the operating system
-if platform.system() == "Linux":
-    import qrcode
-    import wifi
-elif platform.system() == "Windows":
-    import qrcode
-    import wifi
+class WifiQRCodeGeneratorUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Wi-Fi QR Code Generator")
 
-def get_wifi_details():
-    wifi_networks = []
+        # Create labels and entry fields
+        self.ssid_label = tk.Label(self.window, text="Wi-Fi SSID:")
+        self.ssid_entry = tk.Entry(self.window)
+        self.password_label = tk.Label(self.window, text="Wi-Fi Password:")
+        self.password_entry = tk.Entry(self.window, show="*")
+        self.generate_button = tk.Button(self.window, text="Generate QR Code", command=self.generate_qr_code)
 
-    cells = wifi.Cell.all('wlo1')  # Replace 'wlo1' with your Wi-Fi interface name
+        # Position labels and entry fields
+        self.ssid_label.pack()
+        self.ssid_entry.pack()
+        self.password_label.pack()
+        self.password_entry.pack()
+        self.generate_button.pack()
 
-    for cell in cells:
-        ssid = cell.ssid
-        wifi_networks.append(ssid)
+    def get_wifi_details(self):
+        wifi_networks = []
 
-    return wifi_networks
+        # Retrieve Wi-Fi cells
+        wifi_interface = self.get_wifi_interface()
+        cells = wifi.Cell.all(wifi_interface)
 
-def generate_wifi_qr_code(ssid, password, encryption_type):
-    # Wi-Fi network details
-    wifi_data = f'WIFI:S:{ssid};T:{encryption_type};P:{password};;'
+        # Extract SSID of each Wi-Fi network
+        for cell in cells:
+            ssid = cell.ssid
+            wifi_networks.append(ssid)
 
-    # Generate QR code
-    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-    qr.add_data(wifi_data)
-    qr.make(fit=True)
+        return wifi_networks
 
-    # Create an image from the QR code
-    qr_image = qr.make_image(fill_color="black", back_color="white")
+    def get_wifi_interface(self):
+        if platform.system() == "Linux":
+            return "wlo1"  # Default interface name for Linux
+        elif platform.system() == "Windows":
+            return "Wi-Fi"  # Default interface name for Windows
 
-    # Save the QR code image
-    qr_image.save('wifi_qr_code.png')
-    print("Wi-Fi QR code generated successfully.")
+    def generate_qr_code(self):
+        ssid = self.ssid_entry.get()
+        password = self.password_entry.get()
 
-# Retrieve Wi-Fi network details
-networks = get_wifi_details()
+        if ssid == "":
+            messagebox.showerror("Error", "Please enter a Wi-Fi SSID.")
+            return
 
-if len(networks) == 0:
-    print("No Wi-Fi networks found.")
-else:
-    print("Available Wi-Fi networks:")
-    for i, ssid in enumerate(networks):
-        print(f"{i+1}. {ssid}")
+        if password == "":
+            messagebox.showerror("Error", "Please enter a Wi-Fi password.")
+            return
 
-    network_index = int(input("Select a Wi-Fi network (enter the corresponding number): "))
-    password = input("Enter the Wi-Fi password: ")
-
-    if network_index < 1 or network_index > len(networks):
-        print("Invalid network selection.")
-    else:
-        ssid = networks[network_index - 1]
         encryption_type = "WPA"  # Set the encryption type based on your network security
 
-        generate_wifi_qr_code(ssid, password, encryption_type)
+        # Generate and save the QR code
+        try:
+            wifi_data = f'WIFI:S:{ssid};T:{encryption_type};P:{password};;'
+            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+            qr.add_data(wifi_data)
+            qr.make(fit=True)
+            qr_image = qr.make_image(fill_color="black", back_color="white")
+            qr_image.save('wifi_qr_code.png')
+            messagebox.showinfo("Success", "Wi-Fi QR code generated successfully.")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+    def run(self):
+        # Retrieve Wi-Fi network details
+        wifi_networks = self.get_wifi_details()
+
+        if len(wifi_networks) == 0:
+            messagebox.showwarning("Warning", "No Wi-Fi networks found.")
+            self.window.destroy()
+            return
+
+        # Set default SSID
+        self.ssid_entry.insert(0, wifi_networks[0])
+
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    app = WifiQRCodeGeneratorUI()
+    app.run()
